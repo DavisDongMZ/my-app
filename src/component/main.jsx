be712e5e062f3354218bb1cd729297903543874f
DavisDongMZ <mingzhi.dong@mail.utoronto.ca>
2024-04-26 15:27:25 -0400

    first commit

---
+++ src/component/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Main extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+                <center>Welcome to my personal website</center>
+                <p>Hi, I'm Mingzhi Dong, nice to see you</p>
+            </div>
+        );
+    }
+}
+
+export { Main };
